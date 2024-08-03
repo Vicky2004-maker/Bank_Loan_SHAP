ee97133e4819c18d272885095655af521265a499
Vicky2004-maker <vickygamingoff2004@gmail.com>
2024-08-03 15:37:42 +0530

    Initial Commit 3

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, LabelBinarizer
+from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 
 # %%
@@ -45,8 +46,53 @@ plt.grid(axis='y')
 plt.xticks(rotation=20, rotation_mode='anchor', ha='right')
 plt.show()
 
-# %%
+# %% Filling the null values in categorical columns
 
 filter_2 = data[categorical_columns].isna().sum()
+filter_2 = (filter_2[filter_2 != 0])
+print(filter_2)
+print(filter_2.index)
 
-print((filter_2 != 0))
+# %% NAME_TYPE_SUITE FILLING
+name_type_suite_values = (data[filter_2.index[0]]).unique()
+print(name_type_suite_values)
+__d__ = dict()
+
+for i in name_type_suite_values:
+    if isinstance(i, float):
+        continue
+    else:
+        __d__[i] = list(data[filter_2.index[0]]).count(i)
+
+plt.figure(figsize=(12, 10))
+plt.title(f'Frequency Distribution of {filter_2.index[0]}')
+plt.bar(list(__d__.keys()), list(__d__.values()), width=0.5, color='red')
+plt.grid(axis='y')
+plt.xticks(rotation=20, rotation_mode='anchor', ha='right')
+plt.show()
+
+# %%
+
+__k__ = list(__d__.keys())
+__v__ = list(__d__.values())
+if filter_2.index[0] != 'EMERGENCYSTATE_MODE':
+    data[filter_2.index[0]].fillna(__k__[np.argmin(__v__)], inplace=True)
+
+# %% Filling the EMERGENCY STATE MODE
+
+data['EMERGENCYSTATE_MODE'].fillna('No', inplace=True)
+
+# %% Categorical Null values check
+
+# REPEATED CODE (for continuity)
+filter_2 = data[categorical_columns].isna().sum()
+filter_2 = (filter_2[filter_2 != 0])
+print(filter_2)
+print(filter_2.index)
+
+# %% Encoding the Categorical Columns
+
+
+ct = ColumnTransformer(transformers=[
+
+], remainder='passthrough', n_jobs=-1)
